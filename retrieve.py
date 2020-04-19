7190bc4e2945b7a1ab44e09eeb07b61e6b8785df
Nope <nope@nah>
2020-04-19 14:18:10 +0300

    First commit

---
+++ retrieve.py
@@ -0,0 +1,7 @@
+import requests
+print("henlo")
+
+url = 'https://fr.wikisource.org/w/index.php?title=Maximes&printable=yes'
+res = requests.get(url)
+with open('maximes.html', 'wb') as out:
+    out.write(res.text.encode('utf-8'))
