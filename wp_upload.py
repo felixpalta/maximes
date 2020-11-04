79a55b7493588a538ba1e67bb9dcb3ca92d6ae96
nope <nope@nah>
2020-11-04 20:39:08 +0300

    Upload posthumes ok, only must need encoding

--- wp_upload.py
+++ wp_upload.py
@@ -7,13 +7,35 @@ BLOG_ID = '185133231'
 
 API_BASE = 'https://public-api.wordpress.com/rest/v1.1'
 
+CATEGORY_DEFAULT = 'reflexions-morales'
+CATEGORY_SUPPRIMEES = 'maximes-supprimees'
+CATEGORY_POSTHUMES = 'maximes-posthumes'
+
+
+def get_category(category_str):
+    return {
+        'default': CATEGORY_DEFAULT, 'posthumes': CATEGORY_POSTHUMES, 'supprimees': CATEGORY_POSTHUMES
+    }[category_str]
+
+
+def get_slug(category_str, id):
+    return {
+        'default': f'maximes-{id}', 'posthumes': f'posthumes-{id}', 'supprimees': f'supprimees-{id}'
+    }[category_str]
+
+
+def get_title(category_str, id):
+    return {
+        'default': f'Maximes {id}', 'posthumes': f'Posthumes {id}', 'supprimees': f'Supprimées {id}'
+    }[category_str]
+
 
 def make_me_route():
     return API_BASE + "/me"
 
 
 def make_site_route(site_route):
-    return f'{API_BASE}/{BLOG_ID}/sites/{site_route}'
+    return f'{API_BASE}/sites/{BLOG_ID}/{site_route}'
 
 
 def get_token(authfile: str):
@@ -36,17 +58,46 @@ def rq(method: str, url, token, params=None, data=None):
         return requests.post(url, params=params, headers=headers, data=data)
 
 
+def make_post(token, title, content, slug, category=CATEGORY_DEFAULT):
+    url = make_site_route('posts/new')
+    data = {'title': title, 'content': content,
+            'slug': slug, 'categories': [category], 'likes_enabled': False, 'sharing_enabled': False}
+    resp = rq('POST', url, token, data=data)
+    resp.raise_for_status()
+    return resp
+
+
+def post_maxim(one_maxim, token, category_str):
+    id = one_maxim['idx']
+    text = one_maxim['text']
+    resp = make_post(
+        token,
+        title=get_title(category_str, id),
+        content=text,
+        slug=get_slug(category_str, id),
+        category=get_category(category_str)
+    )
+    print(resp.status_code)
+
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--authfile')
+    parser.add_argument('--maximes', required=True)
+    parser.add_argument('--source', required=True,
+                        choices=('default', 'posthumes', 'supprimees'))
     args = parser.parse_args()
     print(args)
     token = get_token(args.authfile)
-    # print('token', token)
 
-    resp = rq('GET', make_me_route(), token)
-    resp.raise_for_status()
-    print(resp.json())
+    with open(args.maximes) as maximes_file:
+        maximes_json = json.load(maximes_file)
+
+    assert maximes_json['source'] == args.source
+    print(f'file opened {args.maximes}, type {args.source}')
+    category_str = args.source
+
+    post_maxim(maximes_json['maximes'][0], token, category_str)
 
 
 main()
